d3bfe7cc80231f322ace02f984046b3e9f146c93
Akshay Biradar <akshay11168@gmail.com>
2024-04-10 14:23:38 +0530

    Added github page

---
+++ docs/browser/chunk-JNXZ4IDH.js
@@ -0,0 +1,1 @@
+import{Q as o,la as i,ma as a,oa as r,pa as s}from"./chunk-LAZVRYSM.js";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-login"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,l){n&1&&(i(0,"p"),r(1,"login works!"),a())}});let t=e;return t})();export{m as LoginComponent};
