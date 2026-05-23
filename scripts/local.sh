c0441a4f2738ff7056d0c06a703f59109b81eea0
Akshay Biradar <akshay11168@gmail.com>
2026-05-24 04:47:17 +0530

    Start afresh

--- scripts/local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd .. &&
-npm i &&
-npm update &&
-npm run build:local
-sudo rm -rf /var/www/biradarakshay/pf/*
-sudo cp -r ./dist/* /var/www/biradarakshay/pf/ &&
-sudo systemctl restart nginx
